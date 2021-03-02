9f630f3485ecd1006c70fb1439e94e897c76aeac
ArlenRPereira <arlen.r.pereira@gmail.com>
2021-03-02 20:05:12 -0300

    Implementing Calculator

---
+++ RestWithASPNET5Udemy/00-RestWithASPNET5Udemy_ScaffoldViaTerminal/Controllers/CalculatorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secoundNumber}")]
+        public IActionResult Get(string firstNumber, string secoundNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secoundNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secoundNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+        
+    }
+}
